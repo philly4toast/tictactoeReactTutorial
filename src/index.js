6af5105ff26499460c8ddc0496c9e64b55b74ca3
Phil Youn <phillipyoun@gmail.com>
2022-03-20 13:34:17 -0700

    Add state to parent Board component

--- src/index.js
+++ src/index.js
@@ -11,14 +11,23 @@ class Square extends React.Component {
   }
   render() {
     return (
-      <button className="square" onClick={()=> console.log('oooh yeah')}>
-        {this.props.value}
+      <button 
+        className="square" 
+        onClick={()=> this.setState({value: 'X'})}
+      >  
+        {this.state.value}
       </button>
-    );
+    )
   }
 }
 
 class Board extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      squares: Array(9).fill(null)
+    }
+  }
   renderSquare(i) {
     return <Square value={i} />;
   }
